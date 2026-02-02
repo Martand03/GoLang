03e2a9bff4739d419d43529630f24ad6724c6db2
Martand Mahajan <martand.mahajan@effigoglobal.com>
2026-02-02 22:10:46 +0530

    starting learning project

---
+++ project-learning/backend/internal/config/db.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := "host=localhost user=admin password=admin dbname=ecommerce port=5434 sslmode=disable"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database")
+	}
+	return db
+}
---
+++ project-learning/backend/internal/user/model.go
@@ -0,0 +1,11 @@
+package user
+
+import "time"
+
+type User struct {
+	ID        uint   `gorm:"primaryKey"`
+	Email     string `gorm:"uniqueIndex; not null"`
+	Password  string `gorm:"not null"`
+	Role      string `gorm:"default:USER"`
+	CreatedAt time.Time
+}
